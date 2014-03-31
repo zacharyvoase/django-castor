0a6d2337383a5100d43039bdfec578078be27dd2
Zachary Voase <zvoase@twitter.com>
2014-03-31 07:09:56 -0700

    Bumped to v0.2.1.

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'django-castor',
-    version          = '0.2.0',
+    version          = '0.2.1',
     author           = "Zachary Voase",
-    author_email     = "zacharyvoase@me.com",
+    author_email     = "z@zacharyvoase.com",
     url              = 'https://github.com/zacharyvoase/django-castor',
     description      = "A content-addressable storage backend for Django.",
     packages         = find_packages(where='.', exclude='test'),
